9edd81686a9c5aa17cc5c818c3ab7998f3c19b6b
Thiago Cauã Mendes De Melo <tmelo_triad@timbrasil.com.br>
2022-06-08 16:23:27 -0300

    Atualização de arquivos css.

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,7 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.css('resources/css/plugins/fontawesome-free/css/all.css', 'public/css/fontawesome.css');
+
 mix.css('resources/css/plugins/icheck-bootstrap/icheck-bootstrap.css', 'public/css');
 mix.css('resources/css/cardapio.css', 'public/css');
 mix.css('resources/css/app.css', 'public/css');
